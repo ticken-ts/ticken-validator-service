f9b5879cfbe013be9043ab247fc0fd2a5505c6c9
facundotorraca <ftorraca@fi.uba.ar>
2023-02-05 14:54:39 -0300

    add validator modelt

---
+++ models/validator.go
@@ -0,0 +1,12 @@
+package models
+
+import "github.com/google/uuid"
+
+type Validator struct {
+	ValidatorID    uuid.UUID `bson:"validator_id"`
+	OrganizationID uuid.UUID `bson:"organization_id"`
+	Firstname      string    `bson:"firstname"`
+	Lastname       string    `bson:"lastname"`
+	Username       string    `bson:"username"`
+	Email          string    `bson:"email"`
+}
