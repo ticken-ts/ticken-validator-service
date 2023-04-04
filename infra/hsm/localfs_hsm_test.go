d0890d5eb5d359aa6fc80ecbcd4da4cf3872f5d8
Javier <javiferreyra97@gmail.com>
2023-04-03 23:45:10 -0300

    fix import in hsm tests

--- infra/hsm/localfs_hsm_test.go
+++ infra/hsm/localfs_hsm_test.go
@@ -2,8 +2,8 @@ package hsm_test
 
 import (
 	"testing"
-	"ticken-event-service/infra/hsm"
-	"ticken-event-service/utils"
+	"ticken-validator-service/infra/hsm"
+	"ticken-validator-service/utils"
 )
 
 const testEncryptionKey = "@NcRfUjXnZr4u7x!A%D*G-KaPdSgVkYp"
